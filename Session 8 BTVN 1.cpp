1fd3656340c3aa8cdbd60df75b1fbce3202f5a0a
Mountain26 <sonseo15072006@gmail.com>
2024-11-28 22:55:59 +0700

    Add files via upload

--- Session 8 BTVN 1.cpp
+++ Session 8 BTVN 1.cpp
@@ -5,8 +5,8 @@ int main() {
     int array[1][2] = {123, 456};
 
     printf("Mang da nhap:\n");
-    for (int i = 0; i < 1; i++) {
-        for (int j = 0; j < 2; j++) {
+    for (int i = 0; i >= 0; i--) {
+        for (int j = 1; j >= 0; j--) {
             printf("%d", array[i][j]);
 		}
     }
